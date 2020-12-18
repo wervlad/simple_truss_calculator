ca2b5f61a0df0b84700dbb6bf4ce8610e83ca23b
Vlad Rusakov <24524555+wervlad@users.noreply.github.com>
2020-12-18 20:25:52 +0400

    refactor truss calculation test

--- features/steps/truss_calculator.py
+++ features/steps/truss_calculator.py
@@ -6,18 +6,10 @@ def load_truss_from_examples(context, filename):
 
 @then("autocalculated results are the same as she caclucated manually")
 def calculate_truss(context):
-    # [
-    # {"id": "PS1", "type": "PinnedSupport", "x": 0.0, "y": 0.0},
-    # {"id": "PS2", "type": "PinnedSupport", "x": 50.0, "y": 0.0},
-    # {"id": "PJ3", "type": "PinJoint", "x": 25.0, "y": 100.0},
-    # {"end1": "PJ3", "end2": "PS1", "id": "B1", "type": "Beam"},
-    # {"end1": "PJ3", "end2": "PS2", "id": "B2", "type": "Beam"},
-    # {"angle": -90.0, "applied_to": "PJ3", "id": "F1", "type": "Force", "value": 5.0}
-    # ]
     results = context.truss_calculator.calculate(context.truss)
-    context.test.assertAlmostEqual(results["B1"], -2.5769410160, decimal=10)
-    context.test.assertAlmostEqual(results["B2"], -2.5769410160, decimal=10)
-    context.test.assertAlmostEqual(results["PS1_x"], 0.6250000000, decimal=10)
-    context.test.assertAlmostEqual(results["PS2_x"], -0.6250000000, decimal=10)
-    context.test.assertAlmostEqual(results["PS1_y"], 2.5000000000, decimal=10)
-    context.test.assertAlmostEqual(results["PS2_y"], 2.5000000000, decimal=10)
+    context.test.assertAlmostEqual(results["B1"],    2.5769, places=4)
+    context.test.assertAlmostEqual(results["B2"],    2.5769, places=4)
+    context.test.assertAlmostEqual(results["PS1x"],  0.6250, places=4)
+    context.test.assertAlmostEqual(results["PS2x"], -0.6250, places=4)
+    context.test.assertAlmostEqual(results["PS1y"],  2.5000, places=4)
+    context.test.assertAlmostEqual(results["PS2y"],  2.5000, places=4)
