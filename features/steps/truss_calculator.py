1a3bcd2930c923c596e1429f06ed28acce22aa6e
Vlad Rusakov <24524555+wervlad@users.noreply.github.com>
2020-12-19 19:43:27 +0400

    add more tests for truss calculation

--- features/steps/truss_calculator.py
+++ features/steps/truss_calculator.py
@@ -4,7 +4,7 @@
 def load_truss_from_examples(context, filename):
     context.truss = context.truss_builder.load_from(filename)
 
-@then("autocalculated results are the same as she caclucated manually")
+@then("results for 1st truss are the same as she caclucated manually")
 def calculate_truss(context):
     results = context.truss_calculator.calculate(context.truss)
     context.test.assertAlmostEqual(results["B1"],    2.5769, places=4)
@@ -18,3 +18,22 @@ def calculate_truss(context):
 def calculate_raises_exception(context, msg):
     with context.test.assertRaisesRegex(ValueError, msg):
         results = context.truss_calculator.calculate(context.truss)
+
+@then("results for 4th truss are the same as she caclucated manually")
+def calculate_truss_4(context):
+    results = context.truss_calculator.calculate(context.truss)
+    context.test.assertAlmostEqual(results["B1"],    2.5769, places=4)
+    context.test.assertAlmostEqual(results["B2"],    2.5769, places=4)
+    context.test.assertAlmostEqual(results["B3"],   -0.6250, places=4)
+    context.test.assertAlmostEqual(results["PS1x"],  0.0000, places=4)
+    context.test.assertAlmostEqual(results["PS1y"],  2.5000, places=4)
+    context.test.assertAlmostEqual(results["RS1"],   2.5000, places=4)
+
+@then("results for 5th truss are the same as she caclucated manually")
+def calculate_truss_5(context):
+    results = context.truss_calculator.calculate(context.truss)
+    context.test.assertAlmostEqual(results["B1"],    3.5355, places=4)
+    context.test.assertAlmostEqual(results["B2"],    3.5355, places=4)
+    context.test.assertAlmostEqual(results["PS1x"],  2.5000, places=4)
+    context.test.assertAlmostEqual(results["PS1y"],  2.5000, places=4)
+    context.test.assertAlmostEqual(results["RS1"],   3.5355, places=4)
--- features/truss_calculator.feature
+++ features/truss_calculator.feature
@@ -3,10 +3,16 @@ Feature: Simple Truss Calculator
 
     Scenario: Calculate trusses
         When Betty loads truss from 'examples/truss01.json'
-        Then autocalculated results are the same as she caclucated manually
+        Then results for 1st truss are the same as she caclucated manually
 
         When Betty loads truss from 'examples/truss02.json'
         Then calculate raises exception 'truss is statically indeterminate'
 
         When Betty loads truss from 'examples/truss03.json'
         Then calculate raises exception 'unbalanced truss'
+
+        When Betty loads truss from 'examples/truss04.json'
+        Then results for 4th truss are the same as she caclucated manually
+
+        When Betty loads truss from 'examples/truss05.json'
+        Then results for 5th truss are the same as she caclucated manually
